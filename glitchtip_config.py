0e94a06cb9c0c8d8becae40e1990fb31b6d0513d
Nanyte25 <freer.mark@gmail.com>
2021-10-13 15:09:29 +0100

    adding place-holder python configuration for glitchtip.

--- glitchtip_config.py
+++ glitchtip_config.py
@@ -9,4 +9,4 @@ import reconcile.utils.gql as gql
 
 from reconcile.github_users import init_github
 from reconcile.utils.secret_reader import SecretReader
-from reconcile.utils.sentry_client import 
+from reconcile.utils.sentry_client import SentryClient
